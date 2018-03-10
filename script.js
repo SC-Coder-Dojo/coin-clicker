323ebde5963b0d6f4f511d9a1125192c43fa9eff
JN6D <JN6D@bigpond.com>
2018-03-10 10:28:56 +1000

    Update script.js

--- script.js
+++ script.js
@@ -22,7 +22,6 @@ $(".colorbox li").on("click",function(){
 });
 $(".money").on("click",function(){
   coins = coins + clickcoin;
-    (money + "c");
   save();
   console.log("clicked", coins, clickcoin);
 $(".total").html(coins);
